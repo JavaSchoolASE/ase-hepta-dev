9d35b742bcd4f7d628f7fe07a121784fea466424
Rosu Dan-Lucian <rosudanlucian94@gmail.com>
2016-03-15 19:02:23 +0200

    Add custom function with a top notch coder name.

--- src/main/java/com/ase/app/App.java
+++ src/main/java/com/ase/app/App.java
@@ -4,10 +4,12 @@ package com.ase.app;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+	public void raduApostoleanu() {
+		System.out.println("Lucian Rosu");
+	}
 }
