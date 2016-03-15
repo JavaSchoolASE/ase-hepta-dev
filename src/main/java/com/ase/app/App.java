81c6ce43825b52f51d8e48a09582c81c45099e71
Adriana Anghel <adriana.anghel@crystal-system.ro>
2016-03-15 19:07:01 +0200

    mesaj

--- src/main/java/com/ase/app/App.java
+++ src/main/java/com/ase/app/App.java
@@ -10,7 +10,14 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
+<<<<<<< HEAD
 public void adriana(){
 	System.out.println(“Adriana Anghel”);
 }
+=======
+	public void AlexandruOprescu(){
+		System.out.println("MERGEEE: Alexandru Oprescu");
+	
+	}
+>>>>>>> 09d8c1fc5057b255c92f89e7b10da49860f3512a
 }
