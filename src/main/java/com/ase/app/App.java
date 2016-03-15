7ab631ec63b7f1dfa3ce65c7026376433d95c2e6
Adriana Anghel <adriana.anghel@crystal-system.ro>
2016-03-15 19:25:27 +0200

    commit changes

--- src/main/java/com/ase/app/App.java
+++ src/main/java/com/ase/app/App.java
@@ -10,14 +10,18 @@ public class App
     {
         System.out.println( "Hello World!" );
     }
-<<<<<<< HEAD
-public void adriana(){
+
+    public void adriana(){
 	System.out.println(“Adriana Anghel”);
 }
-=======
 	public void AlexandruOprescu(){
 		System.out.println("MERGEEE: Alexandru Oprescu");
 	
 	}
->>>>>>> 09d8c1fc5057b255c92f89e7b10da49860f3512a
+
+
+	public void RosuLucian() {
+		System.out.println("Lucian Rosu");
+	}
 }
+
